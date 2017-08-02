98bb94393e7262ca30bb26f48406a12d1542bdaf
Alexander <alexplotbox@gmail.com>
2017-08-02 13:13:02 +0300

    Add mocking for calls

--- index.js
+++ index.js
@@ -1,32 +1,73 @@
 $(document).ready(function() {
-   
     //Mocking Ajax since it doesn't allow requests to file protocol
-
     function AjaxMock(errorMessage, timeout) {
         this.errorCall = function() {
-            return JSON.stringify({
-                status:"error",
-                reason: errorMessage || "Validation errors"
+            return new Promise(function(resolve, reject) {
+                var response = JSON.stringify({
+                    status:"error",
+                    reason: errorMessage || "Validation errors"
+                });
+
+                resolve(response);
             });
+
         }
 
         this.successCall = function() {
-            return JSON.stringify({
-                status: "success"
+            return new Promise(function(resolve, reject) {
+                var response = JSON.stringify({
+                    status: "success"
+                });
+
+                resolve(response);
             });
         }
 
         this.progressCall = function() {
-            return JSON.stringify({
-                status: "progress",
-                timeout: timeout || 1000
-            })
+            return new Promise(function(resolve, reject) {
+                var response = JSON.stringify({
+                    status: "progress",
+                    timeout: timeout || 1000
+                });
+
+                resolve(response);
+            });
         }
     }
 
     const ajaxMock = new AjaxMock();
 
+    var submitButton = $('#submit-button');
 
+    submitButton.click(function(e) {
+        e.preventDefault();
+        var actions = $('#actions').find('div input');
 
+        var selectedAction = actions.filter(function(index, action) {
+            return $(action).prop('checked');
+        })[0];
 
+        switch($(selectedAction).prop('value')) {
+            case 'error': 
+                ajaxMock.errorCall().then(function(res) {
+                    console.log(res);
+                });
+
+                break;
+            case 'success':
+                ajaxMock.successCall().then(function(res) {
+                    console.log(res);
+                });
+
+                break;
+            case 'progress': 
+                ajaxMock.progressCall().then(function(res) {
+                    console.log(res);
+                });
+
+                break;
+            default: 
+                throw Error('No action specified');
+        }
+    });
 });
